0fe76766101bb66a5538e7acaf080449b65d4dc4
Mickael <mickael.bolnet@gmail.com>
2019-10-22 13:25:21 +0200

    consigne jour2 aprem

--- poo/exo_poo.py
+++ poo/exo_poo.py
@@ -7,14 +7,29 @@ from JeuVoiture.Voiture import Voiture
     Créer deux instances de la classe voiture
 """
 #v1 = Voiture([2], [1])
-v1 = Voiture(6, 4)
+#v1 = Voiture(6, 4)
 v2 = Voiture([4,3], [0,-1])
-print(dir(v2))
+#print(dir(v2))
 
-print(v1)
-v1.avancer()
-print(v1)
+print(v2)
+v2.avancer()
+print(v2)
+v2.tourner()
+v2.avancer()
+print(v2)
 
-
+"""
+    Etape 1 : Dans la classe voiture, modifier avancer() de manière à
+    rester toujours entre 0 et 10 sur les deux coordonnées
+    
+    Etape 2 : Créer une classe Board, qui contient une liste 
+    de une voiture. Cette classe une méthode run() qui fait avancer
+    toute les voitures de sa liste_voitures
+    
+    Etape 3: ajouter une methode display à la classe Board pour aficher
+    les voitures sur un board en ASCII-art (les voitures vont être
+    représentées par des croix et quand il n'y a pas de voiture sur une
+    case, on aura un espace)
+"""
 
 
