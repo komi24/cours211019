ab06e1f83a46dd27c27051a4f13e8ba80a2c712a
Mickael <mickael.bolnet@gmail.com>
2019-10-24 17:26:04 +0200

    api gmap

--- web/server.py
+++ web/server.py
@@ -3,6 +3,7 @@ from flask import Flask, render_template, request, redirect
 from uuid import uuid4
 from models import db
 from models.Article import Article
+import os
 
 
 app = Flask(__name__, static_url_path="/public", static_folder="public")
@@ -40,5 +41,20 @@ def form():
     db.session.add(article)
     db.session.commit()
     return redirect("/")
+
+@app.route("/delete/<idx>")
+def delete(idx):
+    # article = Article.query.filter_by(id = id).first()
+    article = Article.query.filter(Article.id == idx).first()
+    db.session.delete(article)
+    db.session.commit()
+    os.remove("./public/images/" + article.image)
+    return redirect("/")
+
+@app.route("/article/<idx>")
+def page_article(idx):
+    article = Article.query.filter(Article.id == idx).first()
+    return render_template("article.html", article=article)
+
 
 app.run(port=5000, debug=True)
