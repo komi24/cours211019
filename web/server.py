d793f819eca8b3aaa2c3267f6e6405131ef3bfae
Mickael <mickael.bolnet@gmail.com>
2019-10-24 14:58:31 +0200

    file input

--- web/server.py
+++ web/server.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from flask import Flask, render_template, request, redirect
+from uuid import uuid4
 
 
 app = Flask(__name__, static_url_path="/public", static_folder="public")
@@ -22,7 +23,19 @@ def contact(name):
 
 @app.route("/form", methods=["POST"])
 def form():
-    print(request.form.get("titre"))
+    """
+        Rajouter dans votre lsite_articles
+        un article du formulaire (titre, prix...)
+    """
+    file_img = request.files["img"]
+    filename = str(uuid4()) + file_img.filename
+    file_img.save(f"public/images/{filename}")
+    liste_articles.append({
+        "title": request.form.get("titre"),
+        "prix": request.form.get("prix"),
+        "description": request.form.get("desc"),
+        "img": filename
+    })
     return redirect("/")
 
 app.run(port=5000, debug=True)
